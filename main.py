054e91979a262d47600ab8095485f10bd8c42843
cufrecillo <cufrecillo@gmail.com>
2019-05-18 10:59:26 +0200

    carrera pygame corredores y nombres

--- main.py
+++ main.py
@@ -1,30 +1,67 @@
 import pygame
 import sys
+import random
+
+class Runner():
+    __customes = ('turtle', 'fish', 'prawn', 'moray', 'octopus')
+    
+    def __init__(self, x=0, y=0):
+        
+        ixCustome = random.randint(0,4)
+        
+        self.custome = pygame.image.load("images/{}.png".format(self.__customes[ixCustome]))
+        self.position = [x,y]
+        self.name = ""
+        
+    def avanzar(self):
+        self.position[0] += random.randint(1, 6)
 
 class Game():
     
-    corredores = []
-    
+    runners = []
+    __posY = (160, 200, 240, 280)
+    __names = ("Rober", "Javi", "Tamara", "Marcos")
+    __startLine = -5
+    __finishLine = 620
     
     def __init__(self):
-        
         self.__screen = pygame.display.set_mode((640, 480))
+        self.__background = pygame.image.load("images/background.png")
         pygame.display.set_caption("Carrera de Bichos")
-        self.background = pygame.image.load("images/background.png")
         
-        
+        for i in range(4):
+            theRunner = Runner(self.__startLine, self.__posY[i])
+            theRunner.name = self.__names[i]
+            self.runners.append(theRunner)
+            
     def competir(self):
         
-        while True:
+        gameOver= False
+        while not gameOver:
             # comprobacion de los eventos
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    gameOver = True
+                    
+            for activeRunner in self.runners:
+                activeRunner.avanzar()
+                if activeRunner.position[0] >= self.__finishLine:
+                    print("El ganador es: {}".format(activeRunner.name))
+                    gameOver = True
+                
+            self.__screen.blit(self.__background, (0, 0))
+            
+            for runner in self.runners:
+                self.__screen.blit(runner.custome, runner.position)
+                
+            # refrescar/renderizar                
+            pygame.display.flip()
+        
+        while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
-            
-            # refrescar/renderizar
-            self.__screen.blit(self.background, (0,0))
-            pygame.display.flip()
             
 if __name__ == '__main__':
     pygame.init()
